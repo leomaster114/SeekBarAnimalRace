7efaf51e7e4f11689e53ecd1525bccf3736bcea5
Nguyễn Văn Hoà <hoanv.ptit@gmail.com>
2020-02-09 09:56:23 +0700

    add 4 file on github

---
+++ app/src/main/java/com/example/seekbaranimalrace/DialogActivity.java
@@ -0,0 +1,14 @@
+package com.example.seekbaranimalrace;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DialogActivity extends Activity {
+    Button btn_login;
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
